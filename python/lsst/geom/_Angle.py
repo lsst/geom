ed2d5c88a04e62bdb7becf92fc091bbc8470eb09
Brian Van Klaveren <bvan@slac.stanford.edu>
2020-09-24 13:37:50 -0700

    Update F811 annotations for py 3.7+3.8

--- python/lsst/geom/_Angle.py
+++ python/lsst/geom/_Angle.py
@@ -25,8 +25,8 @@ from lsst.utils import continueClass
 from ._geom import Angle, AngleUnit
 
 
-@continueClass  # noqa F811
-class AngleUnit:
+@continueClass  # noqa: F811 (FIXME: remove for py 3.8+)
+class AngleUnit:  # noqa: F811
 
     def __mul__(self, other):
         if isinstance(other, (Angle, AngleUnit)):
--- python/lsst/geom/_Box.py
+++ python/lsst/geom/_Box.py
@@ -27,8 +27,8 @@ from lsst.utils import continueClass
 from ._geom import Box2I
 
 
-@continueClass  # noqa: F811
-class Box2I:
+@continueClass  # noqa: F811 (FIXME: remove for py 3.8+)
+class Box2I:  # noqa: F811
 
     def grid(self, dtype=np.int32):
         """Return pair of arrays with the centers of all pixels in the box.
